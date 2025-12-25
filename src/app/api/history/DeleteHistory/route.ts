bc4d8dcdb10c69e8c3007abbaaf4f212736bebb9
Naransolongo <tsetsegulziiocherdene@gmail.com>
2025-12-25 20:28:14 +0800

    ochko

--- src/app/api/history/DeleteHistory/route.ts
+++ src/app/api/history/DeleteHistory/route.ts
@@ -1,5 +1,6 @@
 import { query } from "@/lib/connectDB";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
